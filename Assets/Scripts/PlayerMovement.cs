fa7391d3e91bc9dd00de645b59ca4700bb834621
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2023-01-12 11:53:15 +0100

    Player movement + små fixar
    Player movement finns nu, diagonalt movement skulle kunna förbättras. Förändrade även inställningar på spelaren, gav den rigidbody2d bland annat.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 4.5f;
+    public Rigidbody2D rb;
+    
+    [HideInInspector]
+    public Vector2 Movement;
+    public Vector2 lastMovement;
+
+
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+            Movement.x = Input.GetAxisRaw("Horizontal"); //Gör att "Horizontal" knapparna får spelaren att gå i x-axeln. - Daniel
+            Movement.y = Input.GetAxisRaw("Vertical"); //Gör att "Vertical" knapparna får spelaren att gå i y-axeln. - Daniel        
+  
+        if (Movement.magnitude > 0)
+        {
+            lastMovement = Movement;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+            rb.MovePosition(rb.position + Movement * walkSpeed * Time.fixedDeltaTime); //Använder alla värdena för att få spelaren att förflyttas. - Daniel        
+    }
+}
