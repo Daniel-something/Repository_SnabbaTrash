c0b01e0162f8a230ee5cd0c2c08a3d4d094a874f
NeuGemi <sebastian1.lundberg@gmail.com>
2023-01-16 13:57:45 +0100

    massa grejer

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Av Sebastian
+//Läggs på spelaren. Används för att kontrollera mängden inventory slots, samt vilka som är fulla eller inte
+public class Inventory : MonoBehaviour
+{
+
+    public bool[] isFull; //Checkar om alla slots är fulla eller inte
+    public GameObject[] slots; //Kod för att lägga in Slot UIn i
+
+}
---
+++ Assets/Scripts/PlayerScripts/CameraScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Av Sebastian
+
+public class CameraScroll : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z); //Får Kameran att följa efter spelaren via player.position och sen respektive offset.
+    }
+}
