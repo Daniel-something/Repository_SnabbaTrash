946ed737228413ee3e69df42c616c3da77df6791
JonathanLmao <Jonathan.woldu@elev.ga.lbs.se>
2023-01-18 10:38:08 +0100

    uhhhh
    balls? why eneme not spawn skräp in random place

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+    public GameObject Skräp;
+    public bool spawning;
     public GameObject Skott;
     public Transform firePoint;
     // Start is called before the first frame update
     void Start()
     {
-       
+        StartCoroutine(CreateBonde());
+        spawning = true;
+    }
+
+    IEnumerator CreateBonde() //Kallar på SpawnBonde funktionen varje 1,5 sekunder -William
+    {
+        while (true)
+        {
+            SpawnBonde();
+            yield return new WaitForSeconds(1.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Mouse0)) //&& morotAmmo >= 1)
-        {
-            Instantiate(Skott, firePoint.position, firePoint.rotation);
-            //Ammo--;
-        }
+    }
 
+    void SpawnBonde() //Skapar en bonde slumpmässigt emellan de fyra väggarna -William
+    {
+        Instantiate(Skräp);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
